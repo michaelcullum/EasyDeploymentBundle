e142c4823cc211ff7de2e6472e2ca84782b37719
albertofem <albertofem@gmail.com>
2012-06-27 21:40:21 +0200

    Forgot :)

---
+++ EasyDeploymentBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the EasyDeploymentBundle
+ *
+ * (c) Alberto Fernández <albertofem@gmail.com>
+ *
+ * For the full copyright and license information, please read
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace AFM\Bundle\EasyDeploymentBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * {@inheritDoc}
+ */
+class EasyDeploymentBundle extends Bundle
+{
+}
